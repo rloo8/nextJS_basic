143e3471b2b007bd7fd4bde5fb58027f18092139
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-10-10 13:32:35 +0900

    #1 Link / StyleCSS

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+
+  return (
+    <nav>
+      <Link className={router.pathname === "/" ? "active" : ""} href="/">
+        Home
+      </Link>
+      <Link
+        className={router.pathname === "/about" ? "active" : ""}
+        href="/about"
+      >
+        About
+      </Link>
+      <style jsx global>
+        {`
+          nav {
+            background-color: tomato;
+          }
+          .active {
+            color: white;
+          }
+        `}
+      </style>
+    </nav>
+  );
+}
