45fbefaa3cb112161a82d89db929e6bf70e3a5ee
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-10-12 19:19:03 +0900

    #7 getServerSideProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,11 @@
 import Seo from "@/components/Seo";
-import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch("/movies")).json();
-      setMovies(results);
-    })();
-  }, []);
-
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -26,6 +17,9 @@ export default function Home() {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
@@ -43,4 +37,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3000/movies")
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
